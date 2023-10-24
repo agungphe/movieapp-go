2b8528fad72a6ea51a15ee85adee5f5231d28c8c
agungphe <agungpasific@gmail.com>
2023-10-25 01:40:27 +0700

    setting db

--- config/db.go
+++ config/db.go
@@ -10,11 +10,11 @@ import (
 )
 
 func ConnectDatabase() *gorm.DB {
-	username := utils.Getenv("DATABASE_USERNAME", "root")
-	password := utils.Getenv("DATABASE_PASSWORD", "password")
-	host := utils.Getenv("DATABASE_HOST", "127.0.0.1")
-	port := utils.Getenv("DATABASE_PORT", "3306")
-	database := utils.Getenv("DATABASE_NAME", "database_movie")
+	username := utils.Getenv("MYSQLUSER", "root")
+	password := utils.Getenv("MYSQLPASSWORD", "password")
+	host := utils.Getenv("MYSQLHOST", "127.0.0.1")
+	port := utils.Getenv("MYSQLPORT", "3306")
+	database := utils.Getenv("MYSQLDATABASE", "database_movie")
 
 	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, database)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
