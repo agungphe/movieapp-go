d19b992ecee91d4d9ec2a8c1a7d57d1f13155747
agungphe <agungpasific@gmail.com>
2023-10-25 07:56:20 +0700

    setting db railway

--- config/db.go
+++ config/db.go
@@ -11,10 +11,10 @@ import (
 
 func ConnectDatabase() *gorm.DB {
 	username := utils.Getenv("MYSQLUSER", "root")
-	password := utils.Getenv("MYSQLPASSWORD", "password")
-	host := utils.Getenv("MYSQLHOST", "127.0.0.1")
-	port := utils.Getenv("MYSQLPORT", "3306")
-	database := utils.Getenv("MYSQLDATABASE", "database_movie")
+	password := utils.Getenv("MYSQLPASSWORD", "7spcpsamj0xb34u$b-wau_v_zsif4fg6")
+	host := utils.Getenv("MYSQLHOST", "monorail.proxy.rlwy.net")
+	port := utils.Getenv("MYSQLPORT", "46731")
+	database := utils.Getenv("MYSQLDATABASE", "railway")
 
 	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, database)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
